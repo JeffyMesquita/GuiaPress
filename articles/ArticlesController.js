1d5287ae863c4a2d128719dba8ad0be7e14fb3ab
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2020-01-10 00:17:20 -0300

    10/01/19 Aula 107

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -57,5 +57,22 @@ router.post("/articles/delete", (req, res) => {
   }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id;
+  Article.findByPk(id).then(article => {
+    if(article != undefined){
+
+      Category.findAll().then(categories => {
+
+        res.render("admin/articles/edit", {categories: categories});
+      });
+    }else{
+      res.redirect("/");
+    }
+  }).catch(err => {
+    res.redirect("/");
+  });
+});
+
 
 module.exports = router;
